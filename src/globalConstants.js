3a4ff4e2ef6fc984aa53120995562003b6a74dc8
Mortenso Kodehode <mortenso.kodehode@gmail.com>
2022-09-13 10:52:06 +0200

    Initial commit

---
+++ src/globalConstants.js
@@ -0,0 +1,8 @@
+export const APP_HEADING = 'Summer Calculator'
+export const EMPTYSTRING = ''
+export const INITIALSTATE = {cleanOut: false,
+                             display: EMPTYSTRING,
+                             displayLine: EMPTYSTRING,
+                             record: [] }
+export const IROUNDFACTOR = 100000  // Faktor for avrunding til 5 desimaler
+
